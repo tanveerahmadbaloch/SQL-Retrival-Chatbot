ee168ddb1b351c167024cd7e4e011adef1d39187
Tanveer Ahmad Baloch <109783421+tanveerahmadbaloch@users.noreply.github.com>
2024-01-24 19:13:30 +0500

    Add files via upload

---
+++ sql.py
@@ -0,0 +1,38 @@
+import sqlite3
+
+import sqlite3
+
+## Connectt to SQlite
+connection=sqlite3.connect("student.db")
+
+# Create a cursor object to insert record,create table
+
+cursor=connection.cursor()
+
+## create the table
+table_info="""
+Create table STUDENT(NAME VARCHAR(25),CLASS VARCHAR(25),
+SECTION VARCHAR(25),MARKS INT);
+
+"""
+
+cursor.execute(table_info)
+
+## Insert Some more records
+
+cursor.execute('''Insert Into STUDENT values('Tanveer','Data Science','A',90)''')
+cursor.execute('''Insert Into STUDENT values('Ahmad','Data Science','B',100)''')
+cursor.execute('''Insert Into STUDENT values('Amir','Data Science','A',86)''')
+cursor.execute('''Insert Into STUDENT values('Taha','DEVOPS','A',50)''')
+cursor.execute('''Insert Into STUDENT values('Rehman','DEVOPS','A',35)''')
+
+## Disspaly ALl the records
+
+print("The isnerted records are")
+data=cursor.execute('''Select * from STUDENT''')
+for row in data:
+    print(row)
+
+## Commit your changes int he databse
+connection.commit()
+connection.close()
